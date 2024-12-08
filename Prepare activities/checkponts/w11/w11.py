8ca2bcdfd8efbccf935399f52bbbda7762ad901f
neil-buffham <neilbuffham@gmail.com>
2024-12-08 00:40:45 -0700

    file moving

--- Prepare activities/checkponts/w11/w11.py
+++
@@ -1,7 +0,0 @@
-with open("C:/Users/Tato Tower/Documents/Python1/Python1/Prepare activities/checkponts/w11/books.txt") as books_file:
-    for line in books_file:
-        book = line.strip()
-        print(book)
-
-    print()
-    print("End of document")
--- others/first_file.py
+++
@@ -1,3 +0,0 @@
-print('')
-cheese_selection = print('Would you like Cheese?')
-print('')
